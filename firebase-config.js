4de6162972dd59da418fa8bdd8b1ebbab66d0ada
nicola.gentili <nicola.gentili@irst.emr.it>
2026-09-16 10:06:24 +0200

    Add Google sign-in for teachers and prepare UID-based teacher authorization

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXwSeqdBNpCeUr_RbNddtPQhVrT_zwXRI",
+  authDomain: "stanzavirtuale-2026.firebaseapp.com",
+  databaseURL: "https://stanzavirtuale-2026-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "stanzavirtuale-2026",
+  storageBucket: "stanzavirtuale-2026.firebasestorage.app",
+  messagingSenderId: "294515975716",
+  appId: "1:294515975716:web:2c25b7500c315cb6ffdab1",
+  measurementId: "G-CBTQG0ZVKL"
+};
